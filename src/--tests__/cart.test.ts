bd40ce8fa399dd1ee018a10978098ba10b0d7d58
Natasha01013 <natasha01013@yandex.ru>
2024-12-19 14:37:51 +0100

    change tests

--- src/--tests__/cart.test.ts
+++ src/--tests__/cart.test.ts
@@ -1,11 +1,17 @@
 import Cart from '../cart';
 import Movie from '../film';
 
-test('checking Cart', () => {
+test('checking Cart method getMovies', () => {
     let cart = new Cart();
     let film = new Movie('Мстители', 2012, 'США', 'Avengers Assemble', 'фантастика, боевик, фэнтези, приключения', 137);
     cart.addMovie(film);
     expect(cart.getMovies()).toEqual([film]);
+});
+
+test('checking Cart method getTotalMovies', () => {
+    let cart = new Cart();
+    let film = new Movie('Мстители', 2012, 'США', 'Avengers Assemble', 'фантастика, боевик, фэнтези, приключения', 137);
+    cart.addMovie(film);
     expect(cart.getTotalMovies()).toBe(1);
 });
 
